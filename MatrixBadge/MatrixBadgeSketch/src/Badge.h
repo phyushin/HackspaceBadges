138c11a880b0a8befac6c276d93fb8487487da45
phyushin <phyushin@gmail.com>
2016-09-27 21:04:17 +0100

    as the title suggests rewriting the badge to use a badge class instead of a struct because im a masochist

--- MatrixBadge/MatrixBadgeSketch/src/Badge.h
+++ MatrixBadge/MatrixBadgeSketch/src/Badge.h
@@ -1,11 +1,89 @@
 #define idPos   0
 #define mindfreePos 1
 #define choiceMadePos 2
-#define freeMindsBasePos 3 
+#define choicePos 3
+#define freeMindsBasePos 4
 
+class Badge{
+
+private:
+
+  bool m_mindFree;
+  bool m_choiceMade;
+  char m_id;
+  int  m_choice;
+  int  m_freeMinds[30];
+
+public:
+
+  Badge ();
+  Badge(char,bool,bool,int,int);
+  void Set_MindFree(bool);
+  void Set_ChoiceMade(bool);
+  void Set_ID(char);
+  void Set_Choice(int);
+  void Set_FreeMind(int,char);
+
+  bool Get_MindFree();
+  bool Get_ChoiceMade();
+  char Get_ID();
+  int Get_Choice();
+  char Get_FreeMind(int);
+};
+  /*
+  void Set_ChoiceMade(bool p_choiceMade){
+    m_choiceMade = p_choiceMade;
+  }
+
+  void Set_ID(char p_id){
+    m_id = p_id;
+  }
+
+  void Set_Choice(int p_choice){
+    m_choice = p_choice;
+  }
+
+  void Set_freeMinds(int p_index,char val){
+      m_freeMinds[p_index]=val;
+  }
+
+  bool Get_ChoiceMade(){
+    return m_choiceMade;
+  }
+
+  bool Get_MindFree(){
+    return m_mindFree;
+  }
+
+  char Get_ID(){
+    return m_id;
+  }
+
+  int Get_Choice(){
+    return m_choice;
+  }
+
+  int Get_FreeMind(int index){
+    return m_freeMinds[index];
+  }
+
+  Badge::Badge(){
+    m_mindFree=false;
+    m_choiceMade=false;
+    m_id='a';
+    m_choice=1;
+  }
+
+};
+
+Badge::Set_MindFree(bool p_mindFree){
+  m_mindFree = p_mindFree;
+}
 typedef struct {
-      char id;
+    char id;
     bool mindFree;
     bool choiceMade;
+    int choice;
     int freeMinds[30];
   } badge_struct;
+*/
--- MatrixBadge/MatrixBadgeSketch/src/helpers.h
+++ MatrixBadge/MatrixBadgeSketch/src/helpers.h
@@ -1,28 +1,39 @@
 #include <Arduino.h>
 #include <EEPROM.h>
-#include "Badge.h"
-
-badge_struct readStruct(){
-  badge_struct badge;
-  badge.id = EEPROM.read(idPos);
-  Serial.print("initialising\n[badge id]:\n" + badge.id ) ;
-  badge.mindFree = EEPROM.read(mindfreePos);
-  Serial.print("\n[mindfree]:\n" + badge.mindFree );
+#include "Badge.cpp"
+Badge readBadge(){
+Badge return_badge = new Badge();
+/*
+  badge.Set_ID('a');
+  badge.Set_MindFree(false);
+  badge.Set_ChoiceMade(false);
+  badge.choice=1;
+  badge.freeMinds[30];
+*/
+  if (EEPROM.read(idPos)is NULL){
+    badge.Set_ID(EEPROM.read(idPos));
+    Serial.print("initialising Badge ID");
+  }
+  //Serial.print("initialising\n[badge id]:\n" + badge.id ) ;
+  badge.Set_MindFree(EEPROM.read(mindfreePos));
+  //Serial.print("\n[mindfree]:\n" + badge.mindFree );
   badge.choiceMade = EEPROM.read(choiceMadePos);
-  Serial.print("\n[choice made]:\n" + badge.choiceMade );
+  badge.choice = EEPROM.read(choicePos);
+  //Serial.print("\n[choice made]:\n" + badge.choiceMade );
   Serial.print("\nBadges Seen {");
   for (int i = 0;i<30;i++){
     badge.freeMinds[i] = EEPROM.read(freeMindsBasePos+i);
   }
   Serial.print("\n}");
-  return badge;
+  return return_badge;
 }
 
-void writeStruct(badge_struct p_badge){
-  EEPROM.write(p_badge.id,idPos);
-  EEPROM.write(p_badge.mindFree,mindfreePos);
-  EEPROM.write(p_badge.choiceMade,choiceMadePos);
+void writeBadge(Badge p_badge){
+  EEPROM.write(p_badge.Get_ID,idPos);
+  EEPROM.write(p_badge.Get_MindFree,mindfreePos);
+  EEPROM.write(p_badge.Get_ChoiceMade,choiceMadePos);
+  EEPROM.write(p_badge.Get_Choice,choicePos);
   for (int i = 0;i<30;i++){
-    EEPROM.write(p_badge.freeMinds[i],freeMindsBasePos+i);
+    EEPROM.write(p_badge.Get_FreeMind[i],freeMindsBasePos+i);
   }
 }
