0fa6beaf0b18d4c3eee9a0004422f42450a12383
phyushin <phyushin@gmail.com>
2016-09-27 21:30:03 +0100

    phyu Y U DO DIS!

--- MatrixBadge/MatrixBadgeSketch/src/Badge.h
+++ MatrixBadge/MatrixBadgeSketch/src/Badge.h
@@ -17,7 +17,7 @@ private:
 public:
 
   Badge ();
-  Badge(char,bool,bool,int,int);
+  Badge(char,bool,bool,int);
   void Set_MindFree(bool);
   void Set_ChoiceMade(bool);
   void Set_ID(char);
@@ -30,60 +30,3 @@ public:
   int Get_Choice();
   char Get_FreeMind(int);
 };
-  /*
-  void Set_ChoiceMade(bool p_choiceMade){
-    m_choiceMade = p_choiceMade;
-  }
-
-  void Set_ID(char p_id){
-    m_id = p_id;
-  }
-
-  void Set_Choice(int p_choice){
-    m_choice = p_choice;
-  }
-
-  void Set_freeMinds(int p_index,char val){
-      m_freeMinds[p_index]=val;
-  }
-
-  bool Get_ChoiceMade(){
-    return m_choiceMade;
-  }
-
-  bool Get_MindFree(){
-    return m_mindFree;
-  }
-
-  char Get_ID(){
-    return m_id;
-  }
-
-  int Get_Choice(){
-    return m_choice;
-  }
-
-  int Get_FreeMind(int index){
-    return m_freeMinds[index];
-  }
-
-  Badge::Badge(){
-    m_mindFree=false;
-    m_choiceMade=false;
-    m_id='a';
-    m_choice=1;
-  }
-
-};
-
-Badge::Set_MindFree(bool p_mindFree){
-  m_mindFree = p_mindFree;
-}
-typedef struct {
-    char id;
-    bool mindFree;
-    bool choiceMade;
-    int choice;
-    int freeMinds[30];
-  } badge_struct;
-*/
--- MatrixBadge/MatrixBadgeSketch/src/helpers.h
+++ MatrixBadge/MatrixBadgeSketch/src/helpers.h
@@ -1,39 +1,28 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 #include "Badge.cpp"
+
 Badge readBadge(){
-Badge return_badge = new Badge();
-/*
-  badge.Set_ID('a');
-  badge.Set_MindFree(false);
-  badge.Set_ChoiceMade(false);
-  badge.choice=1;
-  badge.freeMinds[30];
-*/
-  if (EEPROM.read(idPos)is NULL){
-    badge.Set_ID(EEPROM.read(idPos));
-    Serial.print("initialising Badge ID");
-  }
-  //Serial.print("initialising\n[badge id]:\n" + badge.id ) ;
-  badge.Set_MindFree(EEPROM.read(mindfreePos));
-  //Serial.print("\n[mindfree]:\n" + badge.mindFree );
-  badge.choiceMade = EEPROM.read(choiceMadePos);
-  badge.choice = EEPROM.read(choicePos);
-  //Serial.print("\n[choice made]:\n" + badge.choiceMade );
+  Badge return_badge = new Badge::Badge();
+  return_badge.Set_ID(EEPROM.read(idPos));
+  Serial.print("initialising Badge ID");
+  return_badge.Set_MindFree(EEPROM.read(mindfreePos));
+  return_badge.Set_ChoiceMade(EEPROM.read(choiceMadePos));
+  return_badge.Set_Choice(EEPROM.read(choicePos));
   Serial.print("\nBadges Seen {");
   for (int i = 0;i<30;i++){
-    badge.freeMinds[i] = EEPROM.read(freeMindsBasePos+i);
+    return_badge.Set_FreeMind(i,EEPROM.read(freeMindsBasePos+i));
   }
   Serial.print("\n}");
   return return_badge;
 }
 
 void writeBadge(Badge p_badge){
-  EEPROM.write(p_badge.Get_ID,idPos);
-  EEPROM.write(p_badge.Get_MindFree,mindfreePos);
-  EEPROM.write(p_badge.Get_ChoiceMade,choiceMadePos);
-  EEPROM.write(p_badge.Get_Choice,choicePos);
+  EEPROM.write(p_badge.Get_ID(),idPos);
+  EEPROM.write(p_badge.Get_MindFree(),mindfreePos);
+  EEPROM.write(p_badge.Get_ChoiceMade(),choiceMadePos);
+  EEPROM.write(p_badge.Get_Choice(),choicePos);
   for (int i = 0;i<30;i++){
-    EEPROM.write(p_badge.Get_FreeMind[i],freeMindsBasePos+i);
+    EEPROM.write(p_badge.Get_FreeMind(i),freeMindsBasePos+i);
   }
 }
